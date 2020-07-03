75494dfd05252c12478152cbc7b0b262c33c63cb
khalid-khg <qubakhg@gmail.com>
2020-07-03 04:49:43 -0700

    condtimons

---
+++ condtimons.py
@@ -0,0 +1,6 @@
+username = input('please insert your userName: ')
+password = input('please insert your password: ')
+if username == 'khalid' and password == '1234' :
+    print('Welcome to your email')
+else:
+    print('Error')
