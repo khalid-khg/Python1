35e8a880ead71ab00c4506b00f4ab100b2e21927
khalid-khg <qubakhg@gmail.com>
2020-07-03 05:38:57 -0700

    Dictionri

---
+++ dictionrise.py
@@ -0,0 +1,8 @@
+subjects_with_on_student = {"Arabic":3, "English":3, "Math":2}
+print(subjects_with_on_student)
+student = {"Arabic":["Ali","Mohammad","Mona"], "English":["Jack","Ahmad","Munif"]}
+print(student)
+student['Math'] = ['Salah','Hussein']
+print(student)
+student.update({"History":['Khalid']})
+print(student)
