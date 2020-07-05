34afb37526548b5496504eef4e4f4b9ad684fbb6
khalid-khg <qubakhg@gmail.com>
2020-07-05 11:27:27 -0700

    functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+def liner():
+    print('--------------------------------')
+
+def addtion (num1,num2):
+    print(int(num1) + int(num2))
+
+
+n1 = input('please enter the first number: ')
+n2 = input('please enter the second number: ')
+addtion(n1,n2)
+
+# print('Khalid Khalaf')
+# liner()
+# print(22)
+# liner()
+
