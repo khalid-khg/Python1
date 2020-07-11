e4f7c979129dbfa30d2dac2ac252bf05049c94a9
khalid-khg <qubakhg@gmail.com>
2020-07-11 16:53:53 +0300

    Variables & Data Types

---
+++ Udemy/Dictioary.py
@@ -0,0 +1,4 @@
+x = {"number":1 , "name":"Khalaf" , "age":3}
+print(x["number"])
+print(x["name"])
+print(x["age"]) 
---
+++ Youtube/Variables & Data Types.py
@@ -0,0 +1,8 @@
+character_name = "Khalaf"
+character_age = "3"
+print("there once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " +character_name + ", ")
+print("but didn't like being " + character_name + ".")
