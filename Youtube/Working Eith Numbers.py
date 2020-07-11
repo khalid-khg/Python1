05fcbb2d2d44f68d38a003b92b0302a17c86dd65
khalid-khg <qubakhg@gmail.com>
2020-07-11 17:27:08 +0300

    Working With Numbers

---
+++ Youtube/Working Eith Numbers.py
@@ -0,0 +1,15 @@
+from math import  *
+
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(str(my_num) + " my favorite number")
+my_num = -5
+print(abs(my_num))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
