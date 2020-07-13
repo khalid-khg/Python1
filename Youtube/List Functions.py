f0360514bc41247b653d6cb1fa001feb6029c641
khalid-khg <qubakhg@gmail.com>
2020-07-13 16:54:46 +0300

    List Functions

---
+++ Youtube/List Functions.py
@@ -0,0 +1,22 @@
+lucky_numbers = [4, 8, 15, 23, 42]
+
+friends = ["Nabel", "Emad", "Anwr", "Faris", "Abudallh"]
+
+# friends.extend(lucky_numbers)
+# friends.append("Bader")
+# friends.insert(1, "Saud")
+# friends.remove("Anwr")
+# friends.clear()
+# friends.pop()
+# friends.sort()
+# print(friends)
+# print(friends.index("Faris"))
+
+
+# lucky_numbers.sort()
+# lucky_numbers.reverse()
+# print(lucky_numbers)
+
+friends2 = friends.copy()
+print(friends2)
+
