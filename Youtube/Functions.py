4be54ab0f3ffb0b226571e8e0f16268d8e7c38fe
khalid-khg <qubakhg@gmail.com>
2020-07-13 18:58:37 +0300

    Functions

---
+++ Youtube/Functions.py
@@ -0,0 +1,14 @@
+def say_hi(name, age) :
+    # print("Hello " + name + ", you are " + age)
+    print("Hello " + name + ", you are " + str(age))
+
+# print("Top")
+# say_hi("Khalaf", "3")
+# say_hi("Layla", "53")
+# say_hi("Halimah", "28")
+
+say_hi("Khalaf", 3)
+say_hi("Layla", 53)
+say_hi("Halimah", 28)
+
+# print("Bottom")
